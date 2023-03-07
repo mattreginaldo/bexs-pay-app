6ec1c9f46d33f6b08f407e2271c23656bc1fd62f
mateus <mateusreginaldo1997@gmail.com>
2023-03-07 06:03:47 -0300

    Handling and stylize credit card

--- src/components/CreditCard/index.tsx
+++ src/components/CreditCard/index.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UseFormWatch } from "react-hook-form";
+import classNames from "classnames";
+import creditCardType from "credit-card-type";
 
 import "./styles.scss";
 
 interface Props {
-  className: string;
+  watchFields: UseFormWatch<{
+    number: string;
+    name: string;
+    validateDate: string;
+  }>;
 }
 
-const CreditCard = ({ className, ...rest }: Props) => {
+const INITIAL_STATE = {
+  number: "**** **** **** ****",
+  name: "NOME DO TITULAR",
+  validateDate: "00/00",
+};
+
+const CreditCard = ({ watchFields, ...rest }: Props) => {
+  const watchAllFields = watchFields();
+
+  const { number, name, validateDate } = watchAllFields;
+
+  const [cardType, setCardType] = useState("default");
+
+  const handleWithCardType = (cardNumber: string) => {
+    const formattedCardNumber = cardNumber.replace(/[^A-Z0-9]/gi, "");
+
+    const findCard =
+      creditCardType(formattedCardNumber).length === 1 &&
+      formattedCardNumber.length > 4
+        ? creditCardType(formattedCardNumber)[0].type
+        : "default";
+
+    setCardType(findCard);
+  };
+
+  useEffect(() => {
+    handleWithCardType(number);
+  }, [number]);
+
   return (
-    <div className="credit-card" {...rest}>
+    <div {...rest} className={classNames("credit-card", cardType)}>
       <div className="credit-card__data">
-        <span>**** **** **** ****</span>
+        <span>{number || INITIAL_STATE.number}</span>
         <div className="credit-card__data__name-and-date">
-          <span>NOME DO TITULAR</span>
-          <span>00/00</span>
+          <span>{name || INITIAL_STATE.name}</span>
+          <span>{validateDate || INITIAL_STATE.validateDate}</span>
         </div>
       </div>
     </div>
