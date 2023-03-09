a8a9e13d11567539b6408517d13e4c97a4fee54b
mateus <mateusreginaldo1997@gmail.com>
2023-03-09 18:02:10 -0300

    update inputs and insert errors validation

--- src/components/Inputs/InputText/index.tsx
+++ src/components/Inputs/InputText/index.tsx
@@ -1,46 +1,65 @@
 import React from "react";
-import { Controller, UseControllerProps } from "react-hook-form";
+import {
+  useController,
+  FieldPath,
+  FieldValues,
+  Control,
+} from "react-hook-form";
 import MaskedInput from "react-input-mask";
+import classNames from "classnames";
 
 import "./styles.scss";
 
-interface Props extends UseControllerProps {
+interface InputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> {
+  name: TName;
+  control: Control<TFieldValues>;
   placeholder: string;
-  mask: string;
-  maskChar: string;
-  maxLength: number;
+  mask?: string;
+  maskChar?: string;
+  maxLength?: number;
 }
 
-const InputText = ({
-  placeholder = "Input Text Placeholder",
+const InputText = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  control,
   name,
   mask,
   maskChar,
-  control,
   maxLength,
-}: Props) => (
-  <Controller
-    name={name}
-    control={control}
-    defaultValue=""
-    render={({ field }) => (
-      <div className="input-text">
-        {mask ? (
-          <MaskedInput
-            {...field}
-            mask={mask}
-            maskChar={maskChar || null}
-            placeholder=" "
-            inputRef={field.ref}
-          ></MaskedInput>
-        ) : (
-          <input {...field} maxLength={maxLength} placeholder=" " />
-        )}
-
-        <label>{placeholder}</label>
-      </div>
-    )}
-  />
-);
+  placeholder,
+}: InputProps<TFieldValues, TName>) => {
+  const { field, fieldState } = useController({ control, name });
+  return (
+    <div className={classNames("input-text", { error: !!fieldState.error })}>
+      {mask ? (
+        <MaskedInput
+          {...field}
+          id={name}
+          mask={mask}
+          maskChar={maskChar || null}
+          placeholder=" "
+          inputRef={field.ref}
+        ></MaskedInput>
+      ) : (
+        <input
+          {...field}
+          id={name}
+          ref={field.ref}
+          maxLength={maxLength}
+          placeholder=" "
+        />
+      )}
+      <label>{placeholder}</label>
+      <span className="input-text--error">
+        {fieldState.error && fieldState.error.message}
+      </span>
+    </div>
+  );
+};
 
 export default InputText;
